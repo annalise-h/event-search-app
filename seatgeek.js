bfa8bcecfdb5876019001fd0a89f98baf82685a1
Michael Kuek <mkuek123@gmail.com>
2021-12-19 20:12:42 -0500

    Add functions to seatgeek
    Add functionality for artist search- displays artist name, venue location, and URL for tickets
    Add functionality for city search: displays city, state & start/end dates

--- seatgeek.js
+++ seatgeek.js
@@ -1,5 +1,5 @@
 const key = "client_id=MjUwMDcwMjN8MTYzOTcwNzE2MS43OTAwOTU";
-let searchVal = "dua lipa";
+let searchVal = "billie Eilish";
 let urlifySearch = encodeURIComponent(searchVal);
 let urlifySearch2 = searchVal.replace(" ", "-");
 async function start() {
@@ -7,10 +7,20 @@ async function start() {
     `https://api.seatgeek.com/2/events?performers[primary].slug=${urlifySearch2}&${key}`
   );
   const data = await response.json();
+  let concertTitle = data.events.map((e, index) => {
+    return data.events[index].title;
+  });
+  let venue = data.events.map((e, index) => {
+    return `${data.events[index].venue.name}, ${data.events[index].venue.display_location}`;
+  });
+  let url = data.events.map((e, index) => {
+    return data.events[index].url;
+  });
+  console.table([concertTitle, venue, url]);
   console.log(data);
 }
 
-// start();
+start();
 
 async function getGenres() {
   const response = await fetch(
@@ -33,9 +43,9 @@ async function getTaxonomies() {
   const data = await response.json();
   let data2 = data.taxonomies;
   let data3 = data2.map((element, index) => {
-    return data2[index].name.toLowerCase();
+    return data2[index].name.toLowerCase(); //extract titles from event list
   });
-  let data4 = data3.sort((a, b) => (a > b ? 1 : -1));
+  let data4 = data3.sort((a, b) => (a > b ? 1 : -1)); //alphabetize end list
   console.table(data4);
 }
 
@@ -43,17 +53,19 @@ getTaxonomies();
 
 async function searchVenueByLocation() {
   let state = "GA";
-  let city = "savannah";
+  let city = "atlanta";
   let typeOfEvent = "concert";
+  let dateStart = "2021-01-01";
+  let dateEnd = "2021-12-31";
   const response = await fetch(
-    `https://api.seatgeek.com/2/events?per_page=30&taxonomies.name=${typeOfEvent}&venue.state=${state}&venue.city=${city}&${key}`
+    `https://api.seatgeek.com/2/events?per_page=30&taxonomies.name=${typeOfEvent}&venue.state=${state}&venue.city=${city}&datetime_local.gte=${dateStart}&datetime_local.lte=${dateEnd}&${key}`
   );
   const data = await response.json();
   let data2 = data.events;
   let data3 = data2.map((element, index) => {
-    return data2[index].title.toLowerCase();
+    return data2[index].title.toLowerCase(); //extract titles from event list
   });
-  let data4 = data3.sort((a, b) => (a > b ? 1 : -1));
+  let data4 = data3.sort((a, b) => (a > b ? 1 : -1)); //alphabetize end list
   console.table(data4);
 }
 
