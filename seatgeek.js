3589f93e524464df1738809f6f971f820f1dcb61
Genesis Benitez <90431149+GenesisBenitez@users.noreply.github.com>
2022-01-06 15:40:11 -0500

    updating seatgeek branch

--- seatgeek.js
+++ seatgeek.js
@@ -1,73 +1,37 @@
-const key = "client_id=MjUwMDcwMjN8MTYzOTcwNzE2MS43OTAwOTU";
-let searchVal = "billie Eilish";
-let urlifySearch = encodeURIComponent(searchVal);
-let urlifySearch2 = searchVal.replace(" ", "-");
-async function start() {
-  const response = await fetch(
-    `https://api.seatgeek.com/2/events?performers[primary].slug=${urlifySearch2}&${key}`
-  );
+const key = "client_id=MjUwMDcwMjN8MTYzOTcwNzE2MS43OTAwOTU"; //seetgeek API key
 
+//following function takes in variables [Artist name, City, State, Start date, and End date] as inputs.
+//function returns events of type "concert" based on the inputs
+async function seatgeekSearch(artistInput, state, city, dateStart, dateEnd) {
+  //function limted to events of type "concert"
+  const typeOfEvent = "concert";
+  const urlifySearch = artistInput.replace(" ", "-");
+  //returns list of 30 events
+  const response = await fetch(
+    `https://api.seatgeek.com/2/events?per_page=30&performers[primary].slug=${urlifySearch}&taxonomies.name=${typeOfEvent}&venue.state=${state}&venue.city=${city}&datetime_local.gte=${dateStart}&datetime_local.lte=${dateEnd}&${key}`
+  );
   const data = await response.json();
-  let concertTitle = data.events.map((e, index) => {
-    return data.events[index].title;
-  });
-  let venue = data.events.map((e, index) => {
-    return `${data.events[index].venue.name}, ${data.events[index].venue.display_location}`;
-  });
-  let url = data.events.map((e, index) => {
-    return data.events[index].url;
-  });
-  console.table([concertTitle, venue, url]);
   console.log(data);
-}
-
-start();
-
-async function getGenres() {
-  const response = await fetch(
-    `https://api.seatgeek.com/2/genres?per_page=30&${key}`
+  let events = data.events;
+  // get output for requested information
+  let output = events.map(
+    (
+      { title, venue, url, datetime_local, price_low, price_high, image },
+      index
+    ) => {
+      return {
+        artist: data.events[index].performers[0].name, //extract image from event list
+        title: title.toLowerCase(), //extract titles from event list
+        venue: `${venue.name}, ${venue.display_location}`, //extract venue & venue city from event list
+        url, //extract seatgeek link from event list
+        dates: datetime_local, //extract date of event from event list
+        price_low: data.events[index].stats.lowest_price, //extract low price of event from event list
+        price_high: data.events[index].stats.highest_price, //extract high price of event from event list
+        image: data.events[index].performers[0].image, //extract image of artistfrom event list
+      };
+    }
   );
-  const data = await response.json();
-  let data2 = data.genres;
-  let data3 = data2.map((element, index) => {
-    return data2[index].name;
-  });
-  //   console.table(data3);
+  //sort return based on title of event (a-z)
+  let sortedEvents = output.sort((a, b) => (a.title > b.title ? 1 : -1));
+  return sortedEvents;
 }
-
-getGenres();
-
-async function getTaxonomies() {
-  const response = await fetch(
-    `https://api.seatgeek.com/2/taxonomies?per_page=30&${key}`
-  );
-  const data = await response.json();
-  let data2 = data.taxonomies;
-  let data3 = data2.map((element, index) => {
-    return data2[index].name.toLowerCase(); //extract titles from event list
-  });
-  let data4 = data3.sort((a, b) => (a > b ? 1 : -1)); //alphabetize end list
-  console.table(data4);
-}
-
-getTaxonomies();
-
-async function searchVenueByLocation() {
-  let state = "GA";
-  let city = "atlanta";
-  let typeOfEvent = "concert";
-  let dateStart = "2021-01-01";
-  let dateEnd = "2021-12-31";
-  const response = await fetch(
-    `https://api.seatgeek.com/2/events?per_page=30&taxonomies.name=${typeOfEvent}&venue.state=${state}&venue.city=${city}&datetime_local.gte=${dateStart}&datetime_local.lte=${dateEnd}&${key}`
-  );
-  const data = await response.json();
-  let data2 = data.events;
-  let data3 = data2.map((element, index) => {
-    return data2[index].title.toLowerCase(); //extract titles from event list
-  });
-  let data4 = data3.sort((a, b) => (a > b ? 1 : -1)); //alphabetize end list
-  console.table(data4);
-}
-
-searchVenueByLocation();
