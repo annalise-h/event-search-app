eb8c60577fd5266df93113f26204c5aa76edb851
Michael Kuek <mkuek123@gmail.com>
2022-01-06 20:46:25 -0500

    Update Seatgeek Function names
    normalized function naming between seatgeek & ticketmaster. Removed requirements for City State and Date

--- seatgeek.js
+++ seatgeek.js
@@ -6,9 +6,15 @@ async function seatgeekSearch(artistInput, state, city, dateStart, dateEnd) {
   //function limted to events of type "concert"
   const typeOfEvent = "concert";
   const urlifySearch = artistInput.replace(" ", "-");
+  //if any of the inputs from the form are blank, remove them from query. Artist is only required field.
+  let stateSearch = state == "" ? "" : `&venue.state=${state}`;
+  let citySearch = city == "" ? "" : `&venue.city=${city}`;
+  let startDateSearch =
+    dateStart == "" ? "" : `&datetime_local.gte=${dateStart}`;
+  let endDateSearch = dateEnd == "" ? "" : `&datetime_local.lte=${dateEnd}`;
   //returns list of 30 events
   const response = await fetch(
-    `https://api.seatgeek.com/2/events?per_page=30&performers[primary].slug=${urlifySearch}&taxonomies.name=${typeOfEvent}&venue.state=${state}&venue.city=${city}&datetime_local.gte=${dateStart}&datetime_local.lte=${dateEnd}&${key}`
+    `https://api.seatgeek.com/2/events?per_page=30&performers[primary].slug=${urlifySearch}&taxonomies.name=${typeOfEvent}&${stateSearch}${citySearch}${startDateSearch}${endDateSearch}&${key}`
   );
   const data = await response.json();
   console.log(data);
@@ -19,14 +25,16 @@ async function seatgeekSearch(artistInput, state, city, dateStart, dateEnd) {
       { title, venue, url, datetime_local, price_low, price_high, image },
       index
     ) => {
+      let dateFull = datetime_local.split("T");
       return {
         artist: data.events[index].performers[0].name, //extract image from event list
         title: title.toLowerCase(), //extract titles from event list
         venue: `${venue.name}, ${venue.display_location}`, //extract venue & venue city from event list
         url, //extract seatgeek link from event list
-        dates: datetime_local, //extract date of event from event list
-        price_low: data.events[index].stats.lowest_price, //extract low price of event from event list
-        price_high: data.events[index].stats.highest_price, //extract high price of event from event list
+        date: dateFull[0], //extract date of event from event list
+        time: dateFull[1],
+        price_min: data.events[index].stats.lowest_price, //extract low price of event from event list
+        price_max: data.events[index].stats.highest_price, //extract high price of event from event list
         image: data.events[index].performers[0].image, //extract image of artistfrom event list
       };
     }
